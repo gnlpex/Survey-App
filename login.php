0c77c3e4e02470c0c8ba8683ee3f4aac13c4d5d2
Ginel <ginelexaltado@gmail.com>
2017-11-17 07:56:36 +0800

    First

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<title>Login Page</title>
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	</head>
+	
+	<style>
+		.container {
+			margin: 0 auto;
+			text-align: left;
+			width: 800px;
+			margin-top: 150px;
+		}
+	</style>
+<body style="background-color:#0CF">
+
+	<div class="container" >
+		
+		<div class="col-lg-6" style="margin:0px auto" >
+			<div class="jumbotron" >
+				<h3>Please Login</h3>
+				<form>
+				<div class="form-group">
+				<input type="email" class="form-control" placeholder="Enter Username">
+				</div>
+				<div class="form-group">
+				<input type="password" class="form-control" placeholder="Password">
+				</div>
+				<div class="checkbox">
+				<label>
+				<input type="checkbox">
+				Remember me
+				</label>
+				</div>
+				<button type="submit" class="btn btn-primary form-control">Login</button>
+				<br />
+				<br />
+				<label> Not a member yet?</label>
+				<button type="submit" class="btn btn-primary form-control">Sign Up</button>
+				</form>
+			
+			
+			</div>
+		
+		
+		</div>
+		
+		
+		
+	</div>
+</body>
+</html>
